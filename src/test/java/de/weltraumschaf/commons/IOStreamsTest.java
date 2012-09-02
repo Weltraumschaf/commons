92f3b69e0bc5b50572a55cdeadcef94c2883e3ae
Weltraumschaf <ich@weltraumschaf.de>
2012-09-02 23:48:04 +0200

    Initial commit for commons lib.

---
+++ src/test/java/de/weltraumschaf/commons/IOStreamsTest.java
@@ -0,0 +1,63 @@
+/*
+ * LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ */
+
+package de.weltraumschaf.commons;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author sxs
+ */
+public class IOStreamsTest {
+
+    private final IOStreams defaultStreams = IOStreams.newDefault();
+    private final IOStreams mockedStreams = new IOStreams(
+            mock(InputStream.class), mock(PrintStream.class), mock(PrintStream.class));
+
+    @Test public void testGetStderrOnDefault() {
+        assertSame(System.err, defaultStreams.getStderr());
+    }
+
+    @Test public void testGetStdinOnDefault() {
+        assertSame(System.in, defaultStreams.getStdin());
+    }
+
+    @Test public void testGetStdoutOnDefault() {
+        assertSame(System.out, defaultStreams.getStdout());
+    }
+
+    @Test public void println() {
+        final String msg = "some text";
+        mockedStreams.println(msg);
+        verify(mockedStreams.getStdout(), times(1)).println(msg);
+    }
+
+    @Test public void printlnErr() {
+        final String msg = "some text";
+        mockedStreams.printlnErr(msg);
+        verify(mockedStreams.getStderr(), times(1)).println(msg);
+    }
+
+    @Test public void printStackTraceToStdErr() {
+        final PrintStream err = mock(PrintStream.class);
+        final Exception ex = mock(Exception.class);
+        final IOStreams streams = new IOStreams(mock(InputStream.class), mock(PrintStream.class), err);
+
+        streams.printStackTraceToStdErr(ex);
+        verify(ex, times(1)).printStackTrace(err);
+    }
+
+}

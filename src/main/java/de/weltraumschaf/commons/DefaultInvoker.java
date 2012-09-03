77fd2724536fe31729b90004c528720ff0406bfc
Weltraumschaf <ich@weltraumschaf.de>
2012-09-03 15:48:20 +0200

    BAse implementation of command line invokable.

---
+++ src/main/java/de/weltraumschaf/commons/DefaultInvoker.java
@@ -0,0 +1,59 @@
+/*
+ * LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ */
+package de.weltraumschaf.commons;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class DefaultInvoker implements Invokable {
+
+    private final String[] args;
+
+    private final IOStreams ioStreams;
+
+    private final ShutDownHook shutDownHooks = new ShutDownHook();
+
+    public DefaultInvoker(final String[] args, final IOStreams ioStreams) {
+        this.args = args.clone();
+        this.ioStreams = ioStreams;
+    }
+
+    public static void main(final String[] args) {
+        final IOStreams ioStreams = IOStreams.newDefault();
+
+        try {
+            final Invokable application = new DefaultInvoker(args, ioStreams);
+            application.execute();
+        } catch (Exception ex) {
+            ioStreams.getStderr().println(ex.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    @Override
+    public void execute() {
+        Runtime.getRuntime().addShutdownHook(shutDownHooks);
+    }
+
+    protected String[] getArgs() {
+        return args;
+    }
+
+    protected IOStreams getIoStreams() {
+        return ioStreams;
+    }
+
+    protected ShutDownHook getShutDownHooks() {
+        return shutDownHooks;
+    }
+
+}

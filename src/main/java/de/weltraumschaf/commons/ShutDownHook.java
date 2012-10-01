86cfc56a1ed8f88b9fce943532ed6b63c585883a
Weltraumschaf <ich@weltraumschaf.de>
2012-10-01 23:08:57 +0200

    Make shut down hook package private because it's only used in the invokable adapter.

--- src/main/java/de/weltraumschaf/commons/ShutDownHook.java
+++ src/main/java/de/weltraumschaf/commons/ShutDownHook.java
@@ -34,7 +34,7 @@ import java.util.logging.Logger;
  * </code>
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
-public class ShutDownHook extends Thread {
+class ShutDownHook extends Thread {
 
     /**
      * Default logger for this class.

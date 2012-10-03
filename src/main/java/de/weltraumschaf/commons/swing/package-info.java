5aa621dc9b9ca90deade7366ea3c6643546d7bf1
Weltraumschaf <ich@weltraumschaf.de>
2012-10-03 23:06:40 +0200

    Automatically add license info via maven.

--- src/main/java/de/weltraumschaf/commons/swing/package-info.java
+++ src/main/java/de/weltraumschaf/commons/swing/package-info.java
@@ -1,16 +1,16 @@
 /*
  * LICENSE
  *
- * "THE BEER-WARE LICENSE" (Revision 42):
- * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com> wrote this file.
  * As long as you retain this notice you can do whatever you want with
  * this stuff. If we meet some day, and you think this stuff is worth it,
- * you can buy me a beer in return.
+ * you can buy me a non alcohol free beer in return.
  *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com>
  */
-
 /**
- * HElpers to deal with Swing.
+ * Helpers to deal with Swing.
  *
  * <p>This package is a part of the open-source
  * <a href="https://github.com/Weltraumschaf/commons">Commons</a></p>

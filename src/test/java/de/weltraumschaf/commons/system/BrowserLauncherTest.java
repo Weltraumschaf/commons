9fa1cd5a17eaec7a90793a1cfb2b4e49e65147d0
Weltraumschaf <ich@weltraumschaf.de>
2012-11-04 14:30:48 +0100

    Fix typo in license.

--- src/test/java/de/weltraumschaf/commons/system/BrowserLauncherTest.java
+++ src/test/java/de/weltraumschaf/commons/system/BrowserLauncherTest.java
@@ -5,7 +5,7 @@
  * "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com> wrote this file.
  * As long as you retain this notice you can do whatever you want with
  * this stuff. If we meet some day, and you think this stuff is worth it,
- * you can buy me a non alcohol free beer in return.
+ * you can buy me a non alcohol-free beer in return.
  *
  * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com>
  */
@@ -21,8 +21,8 @@ import org.junit.Ignore;
  */
 public class BrowserLauncherTest {
 
-    @Test @Ignore
-    public void testSomeMethod() {
+    @Test
+    public void openBrowser() {
     }
 
 }
--- src/test/java/de/weltraumschaf/commons/system/OperatingSystemTest.java
+++ src/test/java/de/weltraumschaf/commons/system/OperatingSystemTest.java
@@ -5,14 +5,14 @@
  * "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com> wrote this file.
  * As long as you retain this notice you can do whatever you want with
  * this stuff. If we meet some day, and you think this stuff is worth it,
- * you can buy me a non alcohol free beer in return.
+ * you can buy me a non alcohol-free beer in return.
  *
  * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf(at)googlemail(dot)com>
  */
 package de.weltraumschaf.commons.system;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *

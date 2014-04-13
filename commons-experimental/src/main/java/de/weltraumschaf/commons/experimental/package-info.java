f0fece80c72d8ea0248ff30699eda8602748eece
Sven Strittmatter <ich@weltraumschaf.de>
2014-04-13 21:40:30 +0200

    Add missing package info to experimental package.

---
+++ commons-experimental/src/main/java/de/weltraumschaf/commons/experimental/package-info.java
@@ -0,0 +1,20 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+/**
+ * This package contains experimental API.
+ *
+ * <p>
+ * This API may change dramatically w/o backward compatibility and maybe vanish completely.
+ * </p>
+ */
+package de.weltraumschaf.commons.experimental;

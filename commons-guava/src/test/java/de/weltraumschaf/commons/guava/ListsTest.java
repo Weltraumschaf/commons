e79a8538ded0b0155c3db4171eec425041ffbc91
Sven Strittmatter <ich@weltraumschaf.de>
2014-04-13 23:12:50 +0200

    Tests for the rest of the factory.

--- commons-guava/src/test/java/de/weltraumschaf/commons/guava/ListsTest.java
+++ commons-guava/src/test/java/de/weltraumschaf/commons/guava/ListsTest.java
@@ -14,6 +14,8 @@ package de.weltraumschaf.commons.guava;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -80,11 +82,95 @@ public class ListsTest {
     }
 
     public void newArrayList_Iterable_iterableInput() {
+        final Iterable<String> input = new IterableStub("foo", "bar", "baz");
 
+        final List<String> l1 = Lists.newArrayList(input);
+        assertThat(l1, is(not(nullValue())));
+        assertThat(l1, is(instanceOf(ArrayList.class)));
+        assertThat(l1, is(equalTo(input)));
+        assertThat(l1, is(not(sameInstance(input))));
+
+        final List<String> l2 = Lists.newArrayList(input);
+        assertThat(l2, is(not(nullValue())));
+        assertThat(l2, is(instanceOf(ArrayList.class)));
+        assertThat(l2, is(not(sameInstance(l1))));
+        assertThat(l2, is(equalTo(input)));
+        assertThat(l2, is(not(sameInstance(input))));
+
+        final List<String> l3 = Lists.newArrayList(input);
+        assertThat(l3, is(not(nullValue())));
+        assertThat(l3, is(instanceOf(ArrayList.class)));
+        assertThat(l3, is(not(sameInstance(l1))));
+        assertThat(l3, is(not(sameInstance(l2))));
+        assertThat(l3, is(equalTo(input)));
+        assertThat(l3, is(not(sameInstance(input))));
     }
 
     @Test
     public void newArrayList_Iterator() {
+        final List<String> input = Arrays.asList("foo", "bar", "baz");
+
+        final List<String> l1 = Lists.newArrayList(new IteratorStub(input.iterator()));
+        assertThat(l1, is(not(nullValue())));
+        assertThat(l1, is(instanceOf(ArrayList.class)));
+        assertThat(l1, is(equalTo(input)));
+        assertThat(l1, is(not(sameInstance(input))));
+
+        final List<String> l2 = Lists.newArrayList(new IteratorStub(input.iterator()));
+        assertThat(l2, is(not(nullValue())));
+        assertThat(l2, is(instanceOf(ArrayList.class)));
+        assertThat(l2, is(not(sameInstance(l1))));
+        assertThat(l2, is(equalTo(input)));
+        assertThat(l2, is(not(sameInstance(input))));
+
+        final List<String> l3 = Lists.newArrayList(new IteratorStub(input.iterator()));
+        assertThat(l3, is(not(nullValue())));
+        assertThat(l3, is(instanceOf(ArrayList.class)));
+        assertThat(l3, is(not(sameInstance(l1))));
+        assertThat(l3, is(not(sameInstance(l2))));
+        assertThat(l3, is(equalTo(input)));
+        assertThat(l3, is(not(sameInstance(input))));
     }
 
+    private static final class IterableStub implements Iterable<String> {
+
+        private final Collection<String> data;
+
+        public IterableStub(final String ... values) {
+            super();
+            this.data = Arrays.asList(values);
+        }
+
+        @Override
+        public Iterator<String> iterator() {
+            return new IteratorStub(data.iterator());
+        }
+
+    }
+
+    private static final class IteratorStub implements Iterator<String> {
+
+        private final Iterator<String> it;
+
+        public IteratorStub(final Iterator<String> it) {
+            super();
+            this.it = it;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return it.hasNext();
+        }
+
+        @Override
+        public String next() {
+            return it.next();
+        }
+
+        @Override
+        public void remove() {
+            it.remove();
+        }
+
+    }
 }

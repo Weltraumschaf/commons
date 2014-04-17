ed5de6b6bec67adcb9c3fef426f3dc0df36fdd0a
Sven Strittmatter <ich@weltraumschaf.de>
2014-04-17 21:59:33 +0200

    Add test stubs wit hTODOs.

---
+++ commons-guava/src/test/java/de/weltraumschaf/commons/guava/ObjectsTest.java
@@ -0,0 +1,63 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+package de.weltraumschaf.commons.guava;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+import org.junit.Ignore;
+
+/**
+ * Tests for {@link Objects}.
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class ObjectsTest {
+
+    @Test(expected = UnsupportedOperationException.class)
+    @Ignore("TODO Add private constructor test.")
+    public void constructorThrowsExcpetion() {
+    }
+
+    @Test
+    @Ignore("TODO Imlement test for Objects#equal()")
+    public void equal() {
+
+    }
+
+    @Test
+    @Ignore("TODO Imlement test for Objects#hashCode(...)")
+    public void testHashCode() {
+
+    }
+
+    @Test
+    @Ignore("TODO Imlement test for Objects#toStringHelper(Object)")
+    public void toStringHelper_object() {
+    }
+
+    @Test
+    @Ignore("TODO Imlement test for Objects#toStringHelper(Class)")
+    public void toStringHelper_class() {
+    }
+
+    @Test
+    @Ignore("TODO Imlement test for Objects#toStringHelper(String)")
+    public void toStringHelper_string() {
+    }
+
+    @Test
+    @Ignore("TODO Imlement test for Objects#checkNotNull(Object)")
+    public void checkNotNull() {
+    }
+
+}

a140d57cd4c7281de0e9ded2d9e1f03b9ac5a63b
Sven Strittmatter <ich@weltraumschaf.de>
2014-04-18 15:38:36 +0200

    Implement objects tests.

--- commons-guava/src/test/java/de/weltraumschaf/commons/guava/ObjectsTest.java
+++ commons-guava/src/test/java/de/weltraumschaf/commons/guava/ObjectsTest.java
@@ -13,12 +13,15 @@ package de.weltraumschaf.commons.guava;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import static org.hamcrest.Matchers.either;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
@@ -47,35 +50,96 @@ public class ObjectsTest {
     }
 
     @Test
-    @Ignore("TODO Imlement test for Objects#equal()")
     public void equal() {
+        assertThat(Objects.equal(null, null), is(true));
+        assertThat(Objects.equal(new Object(), new Object()), is(false));
 
+        final Object o = new Object();
+        assertThat(Objects.equal(o, null), is(false));
+        assertThat(Objects.equal(null, o), is(false));
+        assertThat(Objects.equal(o, o), is(true));
+
+        assertThat(Objects.equal("foo", null), is(false));
+        assertThat(Objects.equal(null, "foo"), is(false));
+        assertThat(Objects.equal("foo", "foo"), is(true));
+        assertThat(Objects.equal("foo", "bar"), is(false));
+
+        assertThat(Objects.equal("foo", o), is(false));
+
+        assertThat(Objects.equal("foo", 1), is(false));
+        assertThat(Objects.equal(1, 2), is(false));
+        assertThat(Objects.equal(1, 1), is(true));
     }
 
     @Test
-    @Ignore("TODO Imlement test for Objects#hashCode(...)")
     public void testHashCode() {
-
+        assertThat(Objects.hashCode(1, true, "foobar"), is(Objects.hashCode(1, true, "foobar")));
+        assertThat(Objects.hashCode(1, true, "foobar"), is(not(Objects.hashCode(1, false, "foobar"))));
     }
 
     @Test
-    @Ignore("TODO Imlement test for Objects#toStringHelper(Object)")
     public void toStringHelper_object() {
+        final Objects.ToStringHelper sut = Objects.toStringHelper(new Object());
+        assertThat(sut.toString(), is(equalTo("Object{}")));
+
+        sut.add("foo", "bar");
+        assertThat(sut.toString(), is(equalTo("Object{foo=bar}")));
+
+        sut.add("snafu", null);
+        assertThat(sut.toString(), is(equalTo("Object{foo=bar, snafu=null}")));
+
+        sut.add("num", 42);
+        assertThat(sut.toString(), is(equalTo("Object{foo=bar, snafu=null, num=42}")));
+
+        sut.add("arr", new ArrayList<Object>());
+        assertThat(sut.toString(), is(equalTo("Object{foo=bar, snafu=null, num=42, arr=[]}")));
     }
 
     @Test
-    @Ignore("TODO Imlement test for Objects#toStringHelper(Class)")
     public void toStringHelper_class() {
+        final Objects.ToStringHelper sut = Objects.toStringHelper(Object.class);
+        assertThat(sut.toString(), is(equalTo("Object{}")));
+
+        sut.add("foo", "bar");
+        assertThat(sut.toString(), is(equalTo("Object{foo=bar}")));
+
+        sut.add("snafu", null);
+        assertThat(sut.toString(), is(equalTo("Object{foo=bar, snafu=null}")));
+
+        sut.add("num", 42);
+        assertThat(sut.toString(), is(equalTo("Object{foo=bar, snafu=null, num=42}")));
+
+        sut.add("arr", new ArrayList<Object>());
+        assertThat(sut.toString(), is(equalTo("Object{foo=bar, snafu=null, num=42, arr=[]}")));
     }
 
     @Test
-    @Ignore("TODO Imlement test for Objects#toStringHelper(String)")
     public void toStringHelper_string() {
+        final Objects.ToStringHelper sut = Objects.toStringHelper("Test");
+        assertThat(sut.toString(), is(equalTo("Test{}")));
+
+        sut.add("foo", "bar");
+        assertThat(sut.toString(), is(equalTo("Test{foo=bar}")));
+
+        sut.add("snafu", null);
+        assertThat(sut.toString(), is(equalTo("Test{foo=bar, snafu=null}")));
+
+        sut.add("num", 42);
+        assertThat(sut.toString(), is(equalTo("Test{foo=bar, snafu=null, num=42}")));
+
+        sut.add("arr", new ArrayList<Object>());
+        assertThat(sut.toString(), is(equalTo("Test{foo=bar, snafu=null, num=42, arr=[]}")));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void checkNotNull_throwsExcpetionForNull() {
+        Objects.checkNotNull(null);
     }
 
     @Test
-    @Ignore("TODO Imlement test for Objects#checkNotNull(Object)")
     public void checkNotNull() {
+        final Object object = new Object();
+        assertThat(Objects.checkNotNull(object), is(sameInstance(object)));
     }
 
 }

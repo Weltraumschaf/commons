3d8464cf7c611d7796739fb2df24cbfa40ebe940
Sven Strittmatter <ich@weltraumschaf.de>
2014-04-18 18:25:20 +0200

    Add some logging.

--- stage-site.sh
+++ stage-site.sh
@@ -4,8 +4,12 @@ CWD=`pwd`
 STAGE="${CWD}/staged-site"
 
 if [ -d "${STAGE}" ] ; then
+  echo "Delete old stage ${STAGE} ..."
   rm -rfv "${STAGE}"
 fi
 
+echo "Make stage: ${STAGE}."
 mkdir -pv "${STAGE}"
-mvn clean install site site:stage -DstagingDirectory="${STAGE}"
+time mvn clean install site site:stage -DstagingDirectory="${STAGE}"
+
+echo "Finished :-)"

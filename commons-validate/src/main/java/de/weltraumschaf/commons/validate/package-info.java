6ccb33eb5e07889d9b774d187f67beb316f80572
Sven Strittmatter <ich@weltraumschaf.de>
2014-04-21 16:25:55 +0200

    Add package info and link to java doc.

---
+++ commons-validate/src/main/java/de/weltraumschaf/commons/validate/package-info.java
@@ -0,0 +1,19 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+/**
+ * Classes to validate input.
+ *
+ * <p>This package is a part of the open-source
+ * <a href="https://github.com/Weltraumschaf/commons">Commons</a></p>
+ */
+package de.weltraumschaf.commons.validate;
--- commons-validate/src/site/markdown/index.md
+++ commons-validate/src/site/markdown/index.md
@@ -4,4 +4,7 @@ Provides classes to validate input. It's like the validate methods from Apache's
 [commongs Lang][commons-lang], but with less load of other classes. This module
 is designed to be very small and only provides validate methods.
 
+See [Validate][validate] class for examples.
+
 [commons-lang]: https://commons.apache.org/proper/commons-lang/
+[validate]:     validate/apidocs/de/weltraumschaf/commons/validate/Validate.html

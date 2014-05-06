a51931cc3ee06fba80706a17fb89d3397b19a848
Sven Strittmatter <ich@weltraumschaf.de>
2014-05-06 13:46:41 +0200

    Add test skeletons.

---
+++ commons-experimental/src/test/java/de/weltraumschaf/commons/experimental/token/BooleanTokenTest.java
@@ -0,0 +1,29 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+package de.weltraumschaf.commons.experimental.token;
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Tests for {@link BooleanToken}.
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class BooleanTokenTest {
+
+    @Rule
+    //CHECKSTYLE:OFF
+    public final ExpectedException thrown = ExpectedException.none();
+    //CHECKSTYLE:ON
+}
---
+++ commons-experimental/src/test/java/de/weltraumschaf/commons/experimental/token/LiteralTokenTest.java
@@ -0,0 +1,28 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+package de.weltraumschaf.commons.experimental.token;
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Tests for {@link LiteralToken}.
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class LiteralTokenTest {
+
+    @Rule
+    //CHECKSTYLE:OFF
+    public final ExpectedException thrown = ExpectedException.none();
+    //CHECKSTYLE:ON
+}

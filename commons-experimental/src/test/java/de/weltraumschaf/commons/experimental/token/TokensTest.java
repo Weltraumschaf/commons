1787e9d55c9919e3d42f3d2f4e5a82ba5577a589
Sven Strittmatter <ich@weltraumschaf.de>
2014-05-06 12:10:03 +0200

    Fix license header.

--- commons-experimental/src/test/java/de/weltraumschaf/commons/experimental/token/TokensTest.java
+++ commons-experimental/src/test/java/de/weltraumschaf/commons/experimental/token/TokensTest.java
@@ -1,7 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
  */
 
 package de.weltraumschaf.commons.experimental.token;

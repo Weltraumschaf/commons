8b4b9d6f78ebe51e411153a89c29fbcfdad56457
Sven Strittmatter <ich@weltraumschaf.de>
2014-08-24 15:05:21 +0200

    Add some common environment names.

--- commons-system/src/main/java/de/weltraumschaf/commons/system/Environments.java
+++ commons-system/src/main/java/de/weltraumschaf/commons/system/Environments.java
@@ -9,7 +9,6 @@
  *
  * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
  */
-
 package de.weltraumschaf.commons.system;
 
 import de.weltraumschaf.commons.validate.Validate;
@@ -41,6 +40,7 @@ public final class Environments {
      * Implementations provide access to read environment variables.
      */
     public interface Env {
+
         /**
          * Get a environment variable.
          *
@@ -48,6 +48,13 @@ public final class Environments {
          * @return never {@code null}, maybe empty
          */
         String get(String name);
+        /**
+         * Get a environment variable.
+         *
+         * @param name must not be {@code null}
+         * @return never {@code null}, maybe empty
+         */
+        String get(Names name);
     }
 
     /**
@@ -60,6 +67,108 @@ public final class Environments {
             Validate.notEmpty(name, "Name must not be null or empty!");
             final String var = System.getenv(name);
             return var == null ? "" : var;
+        }
+
+        @Override
+        public String get(final Names name) {
+            return get(name.getPropertyName());
+        }
+
+    }
+
+    /**
+     * Some common names provided by the JVM.
+     */
+    public static enum Names {
+
+        /**
+         * Character that separates components of a file path.
+         * <p>
+         * This is “/” on UNIX and “\” on Windows.
+         * </p>
+         */
+        FILE_SEPARATOR("file.separator"),
+        /**
+         * Path used to find directories and JAR archives containing class files.
+         * <p>
+         * Elements of the class path are separated by a platform-specific character specified in the path.separator
+         * property.
+         * </p>
+         */
+        CLASS_PATH("java.class.path"),
+        /**
+         * Installation directory for Java Runtime Environment (JRE).
+         */
+        JAVA_HOME("java.home"),
+        /**
+         * JRE vendor name.
+         */
+        JAVA_VENDOR("java.vendor"),
+        /**
+         * JRE vendor URL.
+         */
+        JAVA_VENDOR_URL("java.vendor.url"),
+        /**
+         * JRE version number.
+         */
+        JAVA_VERSION("java.version"),
+        /**
+         * Sequence used by operating system to separate lines in text files.
+         */
+        LINE_SEPARATOR("line.separator"),
+        /**
+         * Operating system architecture.
+         */
+        OS_ARCH("os.arch"),
+        /**
+         * Operating system name.
+         */
+        OS_NAME("os.name"),
+        /**
+         * Operating system version.
+         */
+        OS_VERSION("os.version"),
+        /**
+         * Path separator character used in java.class.path.
+         */
+        OS_PATH_SEPARATOR("path.separator"),
+        /**
+         * User working directory.
+         */
+        USER_DIR("user.dir"),
+        /**
+         * User home directory.
+         */
+        USER_HOME("user.home"),
+        /**
+         * User account name.
+         */
+        USER_NAME("user.name");
+
+        /** The property name. */
+        private final String propertyName;
+
+        /**
+         * Dedicated constructor.
+         *
+         * @param propertyName must not be {@code null} or empty.
+         */
+        private Names(final String propertyName) {
+            this.propertyName = Validate.notEmpty(propertyName, "propertyName");
+        }
+
+        /**
+         * Get the literal property name.
+         *
+         * @return never {@code null} or empty
+         */
+        public String getPropertyName() {
+            return propertyName;
+        }
+
+        @Override
+        public String toString() {
+            return getPropertyName();
         }
 
     }

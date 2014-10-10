921fb4186bcb8a34fa7ad65c89f9a4e907cc2068
Sven Strittmatter <ich@weltraumschaf.de>
2014-10-10 12:14:56 +0200

    Fix warnings and add task marker.

--- commons-time/src/test/java/de/weltraumschaf/commons/time/StopWatchTest.java
+++ commons-time/src/test/java/de/weltraumschaf/commons/time/StopWatchTest.java
@@ -1,17 +1,24 @@
-
-
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
 package de.weltraumschaf.commons.time;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import org.junit.Test;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
 /**
  * Tests for {@link StopWatch}.
  *
+ * TODO Write tests for StopWatch.
+ * 
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
 public class StopWatchTest {

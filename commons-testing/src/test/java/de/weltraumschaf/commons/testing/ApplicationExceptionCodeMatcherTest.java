0f9f7c4914341944935540f5f10645dd0e1a8e5e
Sven Strittmatter <ich@weltraumschaf.de>
2014-10-11 14:30:54 +0200

    Add todo marker.

---
+++ commons-testing/src/test/java/de/weltraumschaf/commons/testing/ApplicationExceptionCodeMatcherTest.java
@@ -0,0 +1,31 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+package de.weltraumschaf.commons.testing;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Tests for {@link ApplicationExceptionCodeMatcher}.
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class ApplicationExceptionCodeMatcherTest {
+
+    @Test
+    public void testSomeMethod() {
+        // TODO Write tests for ApplicationExceptionCodeMatcher.
+    }
+
+}

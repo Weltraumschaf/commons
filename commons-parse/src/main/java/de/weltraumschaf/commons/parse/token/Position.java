a814952d3ca48e38a3a2152282d70f698322d7c0
Sven Strittmatter <ich@weltraumschaf.de>
2015-05-26 01:10:09 +0200

    Add mutator to set column.

--- commons-parse/src/main/java/de/weltraumschaf/commons/parse/token/Position.java
+++ commons-parse/src/main/java/de/weltraumschaf/commons/parse/token/Position.java
@@ -138,9 +138,13 @@ public final class Position {
     public Position incColumn() {
         return new Position(line, column + 1, file);
     }
-    
+
     public Position incLine() {
         return new Position(line + 1, column, file);
+    }
+
+    public Position column(final int col) {
+        return new Position(line, col, file);
     }
 
 }
